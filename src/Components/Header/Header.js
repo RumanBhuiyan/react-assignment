6ab885146c4c4bc54b42c66a3430293fbfeab45e
RumanBhuiyan <rumanbhuiyansany@gmail.com>
2024-05-14 22:12:36 +0600

    configure tailwind css into my project

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div className="mt-5 h-10 flex justify-evenly">
+      <button className="bg-cyan-600 hover:bg-cyan-600 rounded-xl w-1/4">Button 1</button>
+      <button className="bg-cyan-600 hover:bg-cyan-600 rounded-xl w-1/4">Button 2</button>
+      <button className="bg-cyan-600 hover:bg-cyan-600 rounded-xl w-1/4">Button 3</button>
+    </div>
+  );
+}
