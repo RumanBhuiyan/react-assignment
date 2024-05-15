2c27763876c227022468fb66eda667be0d62828f
RumanBhuiyan <rumanbhuiyansany@gmail.com>
2024-05-15 14:32:11 +0600

    complete products page

--- src/Components/Products/Card.js
+++ src/Components/Products/Card.js
@@ -1,44 +1,71 @@
-function Card() {
+function Card(productDetails) {
+  const {
+    title,
+    price,
+    discountPercentage,
+    rating,
+    stock,
+    brand,
+    category,
+    thumbnail,
+    numberOfProducts,
+    addProduct,
+  } = productDetails;
+
+  const addItem = () => {
+    const currentItems = [...numberOfProducts];
+    currentItems.push(productDetails);
+    console.log(currentItems);
+    addProduct((_) => currentItems);
+  };
+
   return (
     <div className="m-5 w-60 bg-zinc-700 shadow-xl shadow-slate-900 rounded-lg text-sm text-white text-center font-medium">
       <div className="p-4 flex flex-col items-center">
-        <img
-          src="https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"
-          alt="product"
-        />
-        <h1 className="mt-2">Title</h1>
+        <img src={thumbnail} alt="product" />
+        <h1 className="mt-2">{title}</h1>
         <p className="mt-2">
-          Price :<span className="ml-2 p-1 bg-rose-600 rounded-md">$99</span>
+          Price :
+          <span className="ml-2 p-1 bg-rose-600 rounded-md">${price}</span>
         </p>
         <p className="mt-2">
-          Discount :<span className="ml-2 p-1 bg-rose-600 rounded-md">12%</span>
+          Discount :
+          <span className="ml-2 p-1 bg-rose-600 rounded-md">
+            {discountPercentage}%
+          </span>
         </p>
         <p className="mt-2">
-          Rating :<span className="ml-2 p-1 bg-rose-600 rounded-md">4.5</span>
+          Rating :
+          <span className="ml-2 p-1 bg-rose-600 rounded-md">{rating}</span>
         </p>
         <p className="mt-2">
-          Stock :<span className="ml-2 p-1 bg-rose-600 rounded-md">94</span>
+          Stock :
+          <span className="ml-2 p-1 bg-rose-600 rounded-md">{stock}</span>
         </p>
         <p className="mt-2">
-          Brand :<span className="ml-2 p-1 bg-rose-600 rounded-md">Apple</span>
+          Brand :
+          <span className="ml-2 p-1 bg-rose-600 rounded-md">{brand}</span>
         </p>
         <p className="mt-2">
           Category :
-          <span className="ml-2 p-1 bg-rose-600 rounded-md">IOS Phone</span>
+          <span className="ml-2 p-1 bg-rose-600 rounded-md">{category}</span>
         </p>
-        <div className="inline-flex items-center mt-2">
-          <button className="fa mr-2">
+        {/* <div className="inline-flex items-center mt-2">
+          <button className="fa mr-2" onClick={()=> ordered_items_controller(prev => prev+1)}>
             <i className="fas fa-plus text-xl"></i>
           </button>
           <span className="bg-yellow-300 inline-flex items-center px-4 py-1 rounded-full text-black">
-            2
+            {ordered_items}
           </span>
-          <button className="fa ml-2">
+          <button className="fa ml-2" onClick={()=> ordered_items_controller(prev => prev-1)}>
             <i className="fas fa-minus text-xl"></i>
           </button>
-        </div>
+        </div> */}
 
-        <button className="py-2 px-4 bg-rose-600 text-white rounded hover:bg-blue-600 active:bg-blue-700 disabled:opacity-50 mt-4 w-full flex items-center justify-center">
+        <button
+          className="py-2 px-4 bg-rose-600 text-white rounded hover:bg-blue-600 active:bg-blue-700 disabled:opacity-50 mt-4 w-full flex items-center justify-center"
+          onClick={addItem}
+        >
           <span className="fa mr-2">
             <i className="fas fa-cart-arrow-down text-xl"></i>
           </span>
--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,9 +1,68 @@
-import Card from "./Card"
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Card from "./Card";
 
 function Products() {
-    return (<div>
-        <Card />
-    </div>);
+  let [products, setProducts] = useState([]);
+  let [buyingProducts, setBuyingProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        let response = await axios.get("https://dummyjson.com/products");
+        setProducts((_) => response.data.products);
+        // console.log(response.data.products);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <>
+      <div className="mt-10 flex justify-center">
+        <span className="fa">
+          <i className="fas fa-bucket text-8xl"></i>
+        </span>
+      </div>
+      <div className="flex justify-center" style={{ marginTop: "-3rem" }}>
+        <span className="p-2 bg-rose-600 text-white rounded-xl">
+          {buyingProducts.length}
+        </span>
+      </div>
+      <div className="flex flex-wrap flex-auto justify-center items-center">
+        {products.length &&
+          products.map(
+            ({
+              id,
+              title,
+              price,
+              discountPercentage,
+              rating,
+              stock,
+              brand,
+              category,
+              thumbnail,
+            }) => (
+              <Card
+                id={id}
+                title={title}
+                price={price}
+                discountPercentage={discountPercentage}
+                rating={rating}
+                stock={stock}
+                brand={brand}
+                category={category}
+                thumbnail={thumbnail}
+                numberOfProducts={buyingProducts}
+                addProduct={setBuyingProducts}
+              />
+            )
+          )}
+      </div>
+    </>
+  );
 }
 
 export default Products;
